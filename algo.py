f58d2cc48f6e64e73f82f063a245cbb102d0e3ac
mohammad-khalilpour <kian.khalilpour@gmail.com>
2023-11-01 13:52:03 +0330

    added BiBFS algorithm code to solve the rubik

--- algo.py
+++ algo.py
@@ -84,6 +84,61 @@ def solve(init_state, init_location, method):
         return current_sol
 
     elif method == 'BiBFS':
-        ...
+        source_fringe = []
+        dest_fringe = []
+        checked_source = {}
+        checked_dest = {}
+
+        checked_source.update({init_state.__str__(): []})
+        checked_dest.update({solved_state().__str__(): []})
+        source_fringe.append([init_state, []])
+        dest_fringe.append([solved_state(), []])
+
+        explored_count = 0
+        expanded_count = 0
+        while True:
+            temp = source_fringe.copy()
+            source_fringe.clear()
+
+            for state in temp:
+                expanded_count += 1
+                current_state = state[0]
+                current_sol = state[1]
+                for i in range(1, 13):
+                    explored_count += 1
+                    n_state = next_state(current_state, i)
+                    if n_state.__str__() in checked_dest:
+                        t = checked_dest.get(n_state.__str__()).tolist()
+                        t.reverse()
+                        print("expanded nodes:" + str(expanded_count))
+                        print("explored nodes:" + str(explored_count))
+                        return np.append(np.append(current_sol, i), t)
+                    if n_state.__str__() not in checked_source:
+                        checked_source.update({n_state.__str__(): np.append(current_sol, i)})
+                        source_fringe.append([n_state, np.append(current_sol, i)])
+
+            temp = dest_fringe.copy()
+            dest_fringe.clear()
+
+            for state in temp:
+                expanded_count += 1
+                current_state = state[0]
+                current_sol = state[1]
+                for i in range(1, 13):
+                    explored_count += 1
+                    n_state = next_state(current_state, i)
+                    q = (i + 6) % 12
+                    if not q:
+                        q = 12
+                    if n_state.__str__() in checked_source:
+                        t = np.append(current_sol, q).tolist()
+                        t.reverse()
+                        print("expanded nodes:" + str(expanded_count))
+                        print("explored nodes:" + str(explored_count))
+                        return np.append(checked_source.get(n_state.__str__()), t)
+                    if n_state.__str__() not in checked_dest:
+                        checked_dest.update({n_state.__str__(): np.append(current_sol, q)})
+                        dest_fringe.append([n_state, np.append(current_sol, q)])
+
     else:
         return []
